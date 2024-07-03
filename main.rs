c178a05fa3319d2b985a51c0497d1c60d9b8d2b6
Charlemagn0x <163346487+Charlemagn0x@users.noreply.github.com>
2024-07-03 09:41:31 +0300

    added new function

--- main.rs
+++ main.rs
@@ -1,12 +1,29 @@
+use actix_web::{web, HttpResponse};
+use crate::AppState;
+
+async fn get_db_pool_status(data: web::Data<AppState>) -> HttpResponse {
+    let state = data.get_ref();
+    let conn = state.pool.get();
+    match conn {
+        Ok(_) => HttpResponse::Ok().json("Database connection is OK"),
+        Err(_) => HttpResponse::Ok().json("Database connection is Not OK"),
+    }
+}
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/status").route(web::get().to(get_db_pool_status)));
+}
+```
+
+```rust
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
 use std::env;
 
 mod models;
@@ -15,10 +32,6 @@ mod schema;
 
 pub struct AppState {
     pub pool: r2d2::Pool<ConnectionManager<SqliteConnection>>,
-}
-
-async fn server_status() -> HttpResponse {
-    HttpResponse::Ok().json("Server is running")
 }
 
 #[actix_web::main]
@@ -35,8 +48,7 @@ async fn main() -> std::io::Result<()> {
             .data(AppState {
                 pool: pool.clone(),
             })
-            .route("/status", web::get().to(server_status))
-            .configure(routes::init_routes)
+            .configure(routes::init_zones) // This now includes the server status define
     })
     .bind(("127.0.0.1", 8080))?
     .run()
