dde5beabacd3f59966ca4ed7a20e1b816d861919
Charlemagn0x <163346487+Charlemagn0x@users.noreply.github.com>
2024-07-29 20:06:16 +0300

    function added

--- db.rs
+++ db.rs
@@ -8,6 +8,12 @@ use dotenv::dotenv;
 use std::env;
 use std::error::Error;
 
+#[derive(Insertable)]
+#[table_name="users"]
+struct NewUser<'a> {
+    name: &'a str,
+}
+
 pub fn establish_connection() -> Result<PgConnection, Box<dyn Error>> {
     dotenv().ok();
 
@@ -18,11 +24,27 @@ pub fn establish_connection() -> Result<PgConnection, Box<dyn Error>> {
         .map_err(|_| format!("Error connecting to {}", database_url).into())
 }
 
+pub fn create_user<'a>(conn: &PgConnection, name: &'a str) -> Result<usize, Box<dyn Error>> {
+    use self::schema::users;
+
+    let new_user = NewUser { name };
+
+    diesel::insert_into(users::table)
+        .values(&new_user)
+        .execute(conn)
+        .map_err(Into::into)
+}
+
 fn main() {
     match establish_connection() {
         Ok(connection) => {
             println!("Successfully connected to the database.");
+
+            match create_user(&connection, "NewUserName") {
+                Ok(_) => println!("User created successfully."),
+                Err(e) => println!("Error creating user: {}", e),
+            }
         },
-        Err(e) => println!("Connection error: {}", e)
+        Err(e) => println!("Connection error: {}", e),
     }
 }
